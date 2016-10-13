9fe5062d9a9d99381d7c742ea2b0b574c6552126
Martin Haynes <Martin.Haynes@creative-assembly.com>
2016-10-13 16:51:25 +0100

    Working on porting across the runner

--- express_mount/express_mount.js
+++ express_mount/express_mount.js
@@ -2,8 +2,43 @@
 
     'use strict';
 
-    module.exports = function () {
+    const Eventify = require('katana-kit').Eventify;
 
-    };
+    const APP = "app";
+    const API = "api";
+
+    class ExpressMount extends Eventify {
+        constructor(app) {
+            this._env = null;
+            this._app = app || null;
+        }
+
+        app() {
+           return this._createExpress(APP);
+        }
+
+        api() {
+            return this._createExpress(API);
+        }
+
+        _createExpress(on) {
+            if(null === this[on]) {
+                const express = require('express');
+                this[on] = express();
+
+                if (on === APP ) {
+                    this._env = this[on].get('env');
+                }
+
+                this.trigger(`emount.${on}.created`, [this[on]]);
+            }
+
+            return this[on];
+        }
+    }
+
+    module.exports = function (app) {
+        return new ExpressMount(app);
+    }
 
 })();
